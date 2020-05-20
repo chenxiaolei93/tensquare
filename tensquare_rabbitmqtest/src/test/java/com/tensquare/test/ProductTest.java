d256a55197ae90496c50820f2a35cc4b1ab67d66
“chenxiaolei93” <936317890@qq.com>
2020-05-21 01:34:25 +0800

    项目提交1

---
+++ tensquare_rabbitmqtest/src/test/java/com/tensquare/test/ProductTest.java
@@ -0,0 +1,40 @@
+package com.tensquare.test;
+
+import com.tensquare.rabbit.RabbitApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RabbitApplication.class)
+public class ProductTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSend() {
+        rabbitTemplate.convertAndSend("rabbay111", "我要1111");
+        rabbitTemplate.convertAndSend("rabbay222", "我要2222");
+    }
+
+    /**
+     * 分列模式
+     */
+    @Test
+    public void testFunout() {
+        rabbitTemplate.convertAndSend("chuanzhi", "", "分列模式");
+    }
+
+    /**
+     * 话题模式
+     */
+    @Test
+    public void testTopic() {
+        rabbitTemplate.convertAndSend("topic22", "abc.log", "订阅模式");
+    }
+
+}
